9365cfe2b0c0168280e72a85be08f7d52c1f3850
= <=>
2019-01-24 20:25:23 -0500

    Move the facebook login to fix a failing test

--- src/Auth.js
+++ src/Auth.js
@@ -9,43 +9,34 @@ class Auth extends Component {
       isLoggedIn: false,
       userId: null,
     }
+
+    this.login = this.login.bind(this);
+  }
+
+  login() {
+    window.FB.getLoginStatus((response) => {
+      if (response.status === 'connected') {
+        const userId = response.authResponse.userID;
+        this.setState({isLoggedIn: true});
+        window.FB.api('/me', (response) => {        
+          axios.post('/users/signin', {
+            userId: userId
+          });
+        });
+      } else {
+        this.setState({isLoggedIn: false});
+      }
+
+      console.log(this.state.isLoggedIn);
+    });
   }
 
   componentDidMount() {
-    window.fbAsyncInit = () => {
-      window.FB.init({
-        appId      : process.env.REACT_APP_FACEBOOK_APP_ID,
-        cookie     : true,
-        xfbml      : true,
-        version    : process.env.REACT_APP_FACEBOOK_APP_VERSION
-      });
+    document.addEventListener('FBObjectReady', this.login);
+  }
 
-      window.FB.AppEvents.logPageView();
-
-      window.FB.getLoginStatus((response) => {
-        if (response.status === 'connected') {
-          const userId = response.authResponse.userID;
-          this.setState({isLoggedIn: true});
-          window.FB.api('/me', (response) => {        
-            axios.post('/users/signin', {
-              userId: userId
-            });
-          });
-        } else {
-          this.setState({isLoggedIn: false});
-        }
-
-        console.log(this.state.isLoggedIn);
-      });
-    };
-
-    (function(d, s, id){
-       var js, fjs = d.getElementsByTagName(s)[0];
-       if (d.getElementById(id)) {return;}
-       js = d.createElement(s); js.id = id;
-       js.src = "https://connect.facebook.net/en_US/sdk.js";
-       fjs.parentNode.insertBefore(js, fjs);
-     }(document, 'script', 'facebook-jssdk'));
+  componentWillUnmount() {
+    document.removeEventListener('FBObjectReady', this.login);
   }
 
   logout() {
--- src/index.js
+++ src/index.js
@@ -5,6 +5,26 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
+window.fbAsyncInit = () => {
+  window.FB.init({
+    appId      : process.env.REACT_APP_FACEBOOK_APP_ID,
+    cookie     : true,
+    xfbml      : true,
+    version    : process.env.REACT_APP_FACEBOOK_APP_VERSION
+  });
+
+  const fbInitEvent = new Event('FBObjectReady');
+  document.dispatchEvent(fbInitEvent);
+};
+
+(function(d, s, id){
+   var js, fjs = d.getElementsByTagName(s)[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement(s); js.id = id;
+   js.src = "https://connect.facebook.net/en_US/sdk.js";
+   fjs.parentNode.insertBefore(js, fjs);
+ }(document, 'script', 'facebook-jssdk'));
+
 ReactDOM.render(
   <BrowserRouter>
     <App />
