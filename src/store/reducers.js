9107234cb98a5e90eaae7e9b225f9e93e24114d9
Kris Tokarz <kj.tokarz@gmail.com>
2019-02-03 14:25:14 -0500

    Fix the reducer to assign to property on state

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,6 +1,18 @@
 const initialState = {
     app: {}
 }
+
 export function rootReducer(state = initialState, {type, payload} = {}) {
+    if (type === 'ADD_RECENTLY_VIEWED') {
+        const recentlyViewedArticles = addRecentlyViewedArticle(state.recentlyViewedArticles, payload);
+        return Object.assign({}, state, {
+            recentlyViewedArticles
+        });
+    }
+
     return state;
+}
+
+function addRecentlyViewedArticle(state = [], payload) {
+    return [...state, payload];
 }
